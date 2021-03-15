0ac70a1831821d970552fea51bef3fb9462e94c3
yangzhicheng <2419931696@qq.com>
2021-03-15 20:18:04 +0800

    同步

--- src/main/java/com/example/myleetcode/offer/Solution10II.java
+++ src/main/java/com/example/myleetcode/offer/Solution10II.java
@@ -1,5 +1,20 @@
 package com.example.myleetcode.offer;
 
 public class Solution10II {
-
+    public int numWays(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return 1;
+        }
+        int p = 0;
+        int q = 1;
+        for(int i=0; i<n; i++) {
+            int temp = (p+q)%1000000007;
+            p = q;
+            q = temp;
+        }
+        return q;
+    }
 }
